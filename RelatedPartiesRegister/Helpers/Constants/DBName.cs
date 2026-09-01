983c07451fe99d1928acc62290dce1d7e8b4d810
Hana Mahmutovic-std <hana.mahmutovic-std@raiffeisengroup.ba>
2026-09-01 10:15:01 +0200

    Initial commit

---
+++ RelatedPartiesRegister/Helpers/Constants/DBName.cs
@@ -0,0 +1,8 @@
+namespace RBBH.ConnectedParties.Helpers.Constants
+{
+    public class DBName
+    {
+        public const string IBIS = "IBIS";
+        public const string CONNECTED_PARTIES = "CONNECTED_PARTIES";
+    }
+}
